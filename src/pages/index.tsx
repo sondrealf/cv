41bb682f368cc4865aeed6dc58ff54cd26b0e71d
Johan August Østbye <Johan.August@outlook.com>
2021-12-15 00:24:17 +0100

    fixed print scale and responsive tablet size

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,7 +8,7 @@ import Profile from "../components/Profile"
 const index = () => {
   return (
     <InfoProvider>
-      <Layout className="grid grid-rows-[22.5rem_repeat(3,minmax(8.625rem,_1fr))] print:grid-cols-[repeat(6,minmax(8.625rem,_1fr))] lg:grid-cols-[repeat(7,minmax(8.625rem,_1fr))] grid-cols-[repeat(2,minmax(2.625rem,_1fr))] w-full">
+      <Layout className="grid grid-rows-[22.5rem_repeat(3,minmax(8.625rem,_1fr))] print:grid-rows-[17.5rem_repeat(3,minmax(8.625rem,_1fr))] print:grid-cols-[repeat(7,minmax(8.625rem,_1fr))] lg:grid-cols-[repeat(7,minmax(8.625rem,_1fr))] grid-cols-[repeat(2,minmax(2.625rem,_1fr))] w-full">
         <Profile className="bg-gray-200 col-span-2 row-span-1" />
         <Info className="bg-gray-100 col-span-2 row-start-2 row-span-3 p-4" />
         <Content className=" col-span-2 sm:col-span-5 print:col-span-6 row-span-4 p-4" />
